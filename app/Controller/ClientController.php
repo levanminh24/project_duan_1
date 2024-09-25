c7c069b1f547af7074436bb5c891d2f52d8fcb47
tuaniv <tuanvtph45197@fpt.edu.vn>
2024-09-26 00:35:18 +0700

    san pham theo danh muc

--- app/Controller/ClientController.php
+++ app/Controller/ClientController.php
@@ -17,5 +17,44 @@ if(isset($_GET['act'])){
                 include 'app/view/Client/home.php';
             }
             break;
+            case 'sptheodm':
+                if (isset($_GET['id']) && !empty($_GET['id'])) {
+                    $iddm = $_GET['id'];
+                    if(isset($_POST['submittimkiem'])) $kyw=$_POST['timkiem'];
+                    else $kyw="";
+                    if(isset($_POST['submitlocgia'])){
+                        $giadau=$_POST['giaspdau'];
+                        $giacuoi=$_POST['giaspcuoi'];
+                    }else{
+                        $giadau=0;
+                        $giacuoi=0;
+                    }
+                    // Lấy danh sách sản phẩm theo danh mục và các điều kiện lọc
+                    $list_sp_dm = load_all_spdm($_GET['id'], $kyw, $giadau, $giacuoi, 1); // Giả sử trang hiện tại là 1
+                    $listdm = load_one_spdm($_GET['id']); 
+                }
+            
+                // Gọi view để hiển thị
+                include "app/view/Client/sanpham/sptheodm.php";
+                break;
+                case 'chitietsp':
+                    if (isset($_GET['id']) && ($_GET['id']) > 0) {
+                        $id = $_GET['id'];
+                        $product = load_one_sp($id);
+                        if ($product) {
+                            extract($product);
+                            $namedm =  load_category_name($iddm);
+                            extract($namedm);
+                         $splq = load_sp_lq($product['iddm']);
+                        }
+                    }
+                    include "app/views/Client/sanpham/ctsp.php";
+                    break;
     }
+}else {
+    $list_banner_home = load_banner_home();
+    $list_sp_home =  loadall_spHome();
+    $list_sp_nb = load_spnoibat();
+    $listtintuchome = tintuc();
+    include './app/view/Client/home.php';
 }
--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require_once "app/Model/clientModel/banner.php";
 require_once "app/Model/clientModel/sanpham.php";
 require_once "app/Model/clientModel/danhmuc.php";
 require_once "app/Model/clientModel/tintuc.php";
-
 require_once "app/Model/clientModel/pdo.php";
 require_once "app/view/Client/header.php";
 require_once "app/Controller/ClientController.php";
-
+require_once "app/view/Client/sanpham/sptheodm.php";
 require_once "app/view/Client/footer.php";
+?>
